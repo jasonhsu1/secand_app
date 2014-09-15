ba540dfa6b40aa51bdf7e8543bcef8a4c616417f
Jason <jason_hsu@yes123.com.tw>
2014-09-15 09:01:41 +0800

    Lession01

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 FirstApp::Application.routes.draw do
+  # get "root/home"
   resources :users
+  # This maps / to teh root#home actions
+  root to:'root#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
